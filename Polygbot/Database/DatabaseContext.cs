6045eeb8b29dcd6743554c605741152d6681e5cd
Bre4dGC <glebbreadc@gmail.com>
2024-11-27 23:53:39 +0300

    Create database outline

---
+++ Polygbot/Database/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Polygbot.Models;
+
+namespace Polygbot.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<UserSettings> UserSettings { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=bot.db");
+        }
+    }
+}

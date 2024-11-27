a0108ccdb394eceb32cf52511628f3fe3fd8e487
Bre4dGC <glebbreadc@gmail.com>
2024-11-27 23:54:14 +0300

    Created start and settings commands

---
+++ Polygbot/Handlers/CommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Polygbot.Handlers
+{
+    public class CommandHandler
+    {
+        public static async Task HandleStartCommand(ITelegramBotClient botClient, Message message)
+        {
+            await botClient.SendMessage(message.Chat.Id, "👅 Какой язык ты хочешь изучать?", 
+                replyMarkup: SettingsHandler.GetLanguageKeyboard());
+        }
+
+        public static async Task HandleSettingsCommand(ITelegramBotClient botClient, Message message)
+        {
+            await botClient.SendMessage(message.Chat.Id, "⚙️ Выберите настройки",
+                replyMarkup: SettingsHandler.GetSettingsMenu());
+        }
+    }
+}

a946cfdc806c916b2f64e350b48e5e52270b5300
Bre4dGC <glebbreadc@gmail.com>
2024-11-27 23:55:19 +0300

    Created send daily word ouline

---
+++ Polygbot/Handlers/WordHandler.cs
@@ -0,0 +1,13 @@
+﻿using Polygbot.Models;
+using Telegram.Bot;
+
+namespace Polygbot.Handlers
+{
+    public class WordHandler
+    {
+        public static async Task SendDailyWord(ITelegramBotClient botClient, long chatId, Language language)
+        {
+
+        }
+    }
+}
